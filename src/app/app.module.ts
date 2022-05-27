f5097eb93bf55d74a769b68d3f243381de25c53b
priyankaankireddypalli <85746649+priyankaankireddypalli@users.noreply.github.com>
2022-05-27 11:57:39 +0530

    Add files via upload
    1.Create two new components(manually or with CLI): WarningAlert and SuccessAlert
    2. Output them beneath each other in the AppComponent
    3. Output a warning or success message in the Components
    4. Style the Components appropriately (maybe some red/ green text?)
    
    Use external or internal templates and styles!
    
    Feel free to create more components, nest them into each other or play around with different types of selectors!

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { SuccessAlertComponent } from './successalert/successalert.component';
+import { WarningAlertComponent } from './warningalert/warningalert.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    WarningAlertComponent,
+    SuccessAlertComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
